c256bab373eb535c154135096fbae06ff97d18fb
osha890 <oshavshav@gmail.com>
2025-02-06 17:02:20 +0300

    delete operations foo overriden

--- operations/models.py
+++ operations/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models, transaction
 
 
@@ -19,10 +20,11 @@ class Account(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    type = models.CharField(max_length=7, choices=Type.choices)
     is_default = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}: {self.type}"
 
 
 class Operation(models.Model):
@@ -36,18 +38,26 @@ class Operation(models.Model):
     def __str__(self):
         return f"{self.type}: {self.amount} (date: {self.date})"
 
+    def clean(self):
+        if self.category.type != self.type:
+            raise ValidationError(f"Категория {self.category} предназначена для другого типа операций")
+
+    def update_balance(self):
+        old_operation = Operation.objects.get(pk=self.pk)
+        old_amount = old_operation.amount
+
+        if old_operation.type == Type.INCOME:
+            old_amount *= -1
+
+        old_operation.account.balance += old_amount
+        old_operation.account.save()
+
     def save(self, *args, **kwargs):
+        self.clean()
         is_new = self.pk is None
 
         if not is_new:
-            old_operation = Operation.objects.get(pk=self.pk)
-            old_amount = old_operation.amount
-
-            if old_operation.type == Type.INCOME:
-                old_amount *= -1
-
-            old_operation.account.balance += old_amount
-            old_operation.account.save()
+            self.update_balance()
 
         with transaction.atomic():
             super().save(*args, **kwargs)
@@ -59,3 +69,7 @@ class Operation(models.Model):
 
             self.account.balance += amount
             self.account.save()
+
+    def delete(self, *args, **kwargs):
+        self.update_balance()
+        super().delete(*args, **kwargs)
