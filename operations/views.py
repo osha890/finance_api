6156393a7dfd5867a98ca2d43ddc93884e53e908
osha890 <oshavshav@gmail.com>
2025-02-10 17:30:33 +0300

    action recent

--- operations/views.py
+++ operations/views.py
@@ -1,7 +1,10 @@
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import make_aware, get_current_timezone
+from pyexpat.errors import messages
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from . import messages
 from .filters import OperationFilter
 from .serializers import AccountSerializer, CategorySerializer, OperationSerializer
 from rest_framework import viewsets
@@ -35,11 +38,47 @@ def set_tz(date):
     return date
 
 
+def get_total_amount(queryset):
+    total_amount = 0
+    for operation in queryset:
+        if operation.type == Type.EXPENSE:
+            total_amount -= operation.amount
+        elif operation.type == Type.INCOME:
+            total_amount += operation.amount
+        else:
+            pass
+    return total_amount
+
+
 class OperationViewSet(viewsets.ModelViewSet):
     queryset = Operation.objects.all()
     serializer_class = OperationSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = OperationFilter
+
+    @action(detail=False, methods=['get'])
+    def recent(self, request):
+        queryset = Operation.objects.all()
+        type_param = request.query_params.get('type')
+        if type_param in dict(Type.choices):
+            queryset = queryset.filter(type=type_param)
+
+        count = request.query_params.get('count', 5)
+        try:
+            count = int(count)
+            if count <= 0:
+                return Response({"error": messages.NOT_A_VALID_NUMBER}, status=400)
+        except ValueError:
+            return Response({"error": messages.NOT_A_VALID_NUMBER}, status=400)
+
+        queryset = queryset.order_by('-date')[:count]
+        total_amount = get_total_amount(queryset)
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response({
+            'total_amount': total_amount,
+            'operations': serializer.data
+        })
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -49,6 +88,7 @@ class OperationViewSet(viewsets.ModelViewSet):
         if date_after:
             date_after = set_tz(date_after)
             queryset = queryset.filter(date__gte=date_after)
+
         if date_before:
             date_before = set_tz(date_before)
             queryset = queryset.filter(date__lte=date_before)
@@ -56,17 +96,9 @@ class OperationViewSet(viewsets.ModelViewSet):
         return queryset
 
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+        queryset = self.filter_queryset(self.get_queryset()).order_by('-date')
         serializer = self.get_serializer(queryset, many=True)
-        total_amount = 0
-
-        for operation in queryset:
-            if operation.type == Type.EXPENSE:
-                total_amount -= operation.amount
-            elif operation.type == Type.INCOME:
-                total_amount += operation.amount
-            else:
-                pass
+        total_amount = get_total_amount(queryset)
 
         return Response({
             'total_amount': total_amount,
