815b6f4742ad1da2acf267efcebcfc406d26fc60
osha890 <oshavshav@gmail.com>
2025-03-01 19:16:48 +0300

    change DateFilter to IsoDateTimeFilter in filters

--- operations/filters.py
+++ operations/filters.py
@@ -7,7 +7,8 @@ class OperationFilter(django_filters.FilterSet):
     type = django_filters.CharFilter(field_name='type', lookup_expr='iexact')
     account = django_filters.NumberFilter(field_name='account__id')
     category = django_filters.NumberFilter(field_name='category__id')
-    date = django_filters.DateFilter(field_name='date', lookup_expr='date')
+    # date = django_filters.DateFilter(field_name='date', lookup_expr='date')
+    date = django_filters.IsoDateTimeFilter(field_name='date')
     date_after = django_filters.IsoDateTimeFilter(field_name='date', lookup_expr='gte')
     date_before = django_filters.IsoDateTimeFilter(field_name='date', lookup_expr='lte')
 
