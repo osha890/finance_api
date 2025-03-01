18a2dde17f439bebf36a0762e951864a72ed96b5
osha890 <oshavshav@gmail.com>
2025-03-01 20:20:54 +0300

    add lookup_expr to date filter

--- operations/filters.py
+++ operations/filters.py
@@ -5,12 +5,12 @@ from .models import Operation
 
 class OperationFilter(django_filters.FilterSet):
     type = django_filters.CharFilter(field_name='type', lookup_expr='iexact')
-    account = django_filters.NumberFilter(field_name='account__id')
-    category = django_filters.NumberFilter(field_name='category__id')
     # date = django_filters.DateFilter(field_name='date', lookup_expr='date')
-    date = django_filters.IsoDateTimeFilter(field_name='date')
+    date = django_filters.IsoDateTimeFilter(field_name='date', lookup_expr='date')
     date_after = django_filters.IsoDateTimeFilter(field_name='date', lookup_expr='gte')
     date_before = django_filters.IsoDateTimeFilter(field_name='date', lookup_expr='lte')
+    account = django_filters.NumberFilter(field_name='account__id')
+    category = django_filters.NumberFilter(field_name='category__id')
 
     class Meta:
         model = Operation
