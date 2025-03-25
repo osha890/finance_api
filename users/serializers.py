9b92ed07846a1703b0a656fe1152df073ec6bf7e
osha890 <oshavshav@gmail.com>
2025-03-25 15:42:39 +0300

    update docker

--- users/serializers.py
+++ users/serializers.py
@@ -14,7 +14,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        Token.objects.create(user=user)
+        Token.objects.create(user=user) # Добавление токена для авторизации
         return user
 
 
--- users/views.py
+++ users/views.py
@@ -15,9 +15,11 @@ User = get_user_model()
 
 class RegisterView(APIView):
     def post(self, request):
+        """Регистрация нового пользователя"""
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
+            # Получаем токен
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=HTTP_400_BAD_REQUEST)
@@ -26,10 +28,10 @@ class RegisterView(APIView):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser]  # Работать с пользователями могут только админы
 
 class TokenView(APIView):
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser]  # Получать токены могут только админы
     def get(self, request, *args, **kwargs):
         tokens = Token.objects.all()
         serialized_tokens = [{'user_id': token.user_id, 'key': token.key} for token in tokens]
